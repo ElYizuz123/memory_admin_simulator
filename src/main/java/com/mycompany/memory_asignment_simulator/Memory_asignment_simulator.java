f5fec364f4eb70be620f2cb286b6bd03802941fa
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2023-11-19 14:08:53 -0600

    Basic structure
    All the user inputs

--- src/main/java/com/mycompany/memory_asignment_simulator/Memory_asignment_simulator.java
+++ src/main/java/com/mycompany/memory_asignment_simulator/Memory_asignment_simulator.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.memory_asignment_simulator;
 
-/**
- *
- * @author chuy-
- */
+import java.util.Scanner;
 public class Memory_asignment_simulator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int mS, oSS, n;
+        int sizes[], tR[];
+        Scanner ent = new Scanner(System.in);
+        System.out.print("Inserte el tamaño de la memoria: ");
+        mS=ent.nextInt();
+        System.out.print("Inserte el tamaño del sistema operativo: ");
+        oSS=ent.nextInt();
+        System.out.print("Inserte el número de procesos: ");
+        n=ent.nextInt();
+        sizes = new int[n];
+        tR = new int[n];
+        for(int i=0; i<n; i++){
+            System.out.print("Inserte el tamaño del proceso "+i+": ");
+            sizes[i]=ent.nextInt();
+            System.out.print("Inserte el tiempo ráfaga del proceso "+i+": ");
+            tR[i]=ent.nextInt();
+        }
+        
+        //ent.nextLine();
     }
 }
