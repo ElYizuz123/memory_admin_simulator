46c32678d459e3693e0d7af85926f08e88adb0f7
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2023-11-19 13:06:46 -0600

    First commit

---
+++ src/main/java/com/mycompany/memory_asignment_simulator/Memory_asignment_simulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.memory_asignment_simulator;
+
+/**
+ *
+ * @author chuy-
+ */
+public class Memory_asignment_simulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
